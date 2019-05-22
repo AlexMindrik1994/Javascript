bf146553d8b4f426dd17c61b8d5280ea30484634
AlexMindrik1994 <almin040@gmail.com>
2019-05-22 20:49:52 +0300

    create homework.html

---
+++ js/homework.js
@@ -0,0 +1,27 @@
+/*------Задача 1----------------*/
+/*let sot = 10;
+sot = (sot*100)-(15*25);
+console.log(sot);*/
+
+/*--------Задача 2--------------------------*/
+
+/*let oval = 15;
+let oval_2 = 600;
+oval = oval *100;
+area = oval - oval_2;
+console.log (area);*/
+
+/*-------------------------Задача 3------------------------*/
+
+let a = '03';
+let b = '10';
+let c = '12';
+
+
+if (a < b &&  a < c){
+	console.log (a);
+} else if(b < a && b < c){
+	console.log (b);
+} else if (c < a && c < b){
+	console.log (c);
+}

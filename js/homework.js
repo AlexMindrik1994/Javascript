0d52636492b7f820889c3224329fc35d58a721aa
AlexMindrik1994 <49689983+AlexMindrik1994@users.noreply.github.com>
2019-05-23 12:20:34 +0300

    Add files via upload

--- js/homework.js
+++ js/homework.js
@@ -13,7 +13,7 @@ console.log (area);*/
 
 /*-------------------------Задача 3------------------------*/
 
-let a = '03';
+/*let a = '03';
 let b = '10';
 let c = '12';
 
@@ -22,6 +22,21 @@ if (a < b &&  a < c){
 	console.log (a);
 } else if(b < a && b < c){
 	console.log (b);
-} else if (c < a && c < b){
+} else {
 	console.log (c);
+}*/
+
+
+/*----------------------Задача 4-------------------------------*/
+
+let m = '8';
+let n = '11.45';
+
+chis = m - 10;
+chis_1 = n - 10;
+
+if (chis > chis_1){
+	console.log (m);
+} else {
+	console.log (n);
 }
--- js/homework2.js
+++ js/homework2.js
@@ -1,14 +1,14 @@
 
 /*----------------------------------------------------------Задача 2 ------------------------------------------*/
 
-let ed_dlina =parseInt (prompt ("Выберете единицы длины: \n1 - Дециметр\n2 - Километр\n3 - Метр\n4 -Сантиметр\n5 -Миллиметр "));
+/*let ed_dlina =parseInt (prompt ("Выберете единицы длины: \n1 - Дециметр\n2 - Километр\n3 - Метр\n4 -Сантиметр\n5 -Миллиметр "));
 
 let dlina = parseInt(prompt ("Введите длину"));
 
 
 
 if (ed_dlina === 1){
-	let a = (dlina  * 10);
+	let a = (dlina  * 0.1);
 	alert ((a) + " м");
 }else if (ed_dlina === 2){
 	let b = (dlina  * 1000);
@@ -22,7 +22,7 @@ if (ed_dlina === 1){
 } else if (ed_dlina === 5){
 	let e = (dlina * 0.001);
 	alert ((e) + " м");
-}
+}*/
 
 
 /*------------------------------------Задача 1-----------------------------------------------------------------*/
@@ -31,31 +31,31 @@ if (ed_dlina === 1){
 /*let month = prompt (" Введите номер месяца");
 
 if (month === "1"){
-	alert ("Январь");
+	alert ("Зима");
 } else if (month === "2"){
-	alert ("Февраль");
+	alert ("Зима");
 } else if (month === "3"){
-	alert ("Март");
+	alert ("Весна");
 } else if (month === "4"){
-	alert ("Апрель");
+	alert ("Весна");
 }else if (month === "5"){
-	alert ("Май");
+	alert ("Весна");
 }else if (month === "6"){
-	alert ("Июнь");
+	alert ("Лето");
 }else if (month === "7"){
-	alert ("Июль");
+	alert ("Лето");
 }else if (month === "8"){
-	alert ("Август");
+	alert ("Лето");
 }else if (month === "9"){
-	alert ("Сентябрь");
+	alert ("Осень");
 }else if (month === "10"){
-	alert ("Октябрь");
+	alert ("Осень");
 }else if (month === "11"){
-	alert ("Ноябрь");
+	alert ("Осень");
 }else if (month === "12"){
-	alert ("Декабрь");
-}*/
-
+	alert ("Зима");
+}
+*/
 
 
 
@@ -68,21 +68,47 @@ if (month === "1"){
 
 /*let number = prompt ("Введите число от -999 до 999");
 
-if ((number >= -999) && (number <= -100)) {
+if (number >= -999 && number <= -100) {
 	alert ("Число отрицательное трехзначное");
-} else if  ((number >= -99) && (number <= -10)){
+} else if  (number >= -99 && number <= -10){
 	alert ("Число отрицательное двухзначное");
-} else if ((number >= -9) && (number <= -1)){
+} else if (number >= -9 && number <= -1){
 	alert ("Число отрицательное односоставное");
 } else if (number === 0){
 	alert ("Число нулевое");
-} else if ((number >= 1) && (number <= 9)){
+} else if (number >= 1 && number <= 9){
 	alert ("Число положительное односоставное");	
-} else if ((number >= 10) && (number <= 99)){
+} else if (number >= 10 && number <= 99){
 	alert ("Число положительное двухсоставное");
-} else if((number >= 100) && (number <= 999)){
+} else if (number >= 100 && number <= 999)){
 	alert ("Число положительное трехсоставное");
 }
 */
 
 
+/*let arr = [];
+let A = [12,4,3,10,1,20];
+let B = [-3,-7,-100,-33];
+
+let C = A.concat(B);
+console.log (C);*/
+
+let D = [-12,4,3,10,1,20];
+
+/*let min = D.sort ().shift ();
+console.log (D);
+
+let max = D.sort ().pop ();
+console.log (D);*/
+
+min = D[0];
+max = D[0];
+
+for (let i =0; i < D.length; i++){
+	if (min > D[i]){
+		D.shift (D[i]);
+	} else if (max < D[i]){
+		D.pop (D[i]);
+	}
+}
+console.log (D);

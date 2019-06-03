4f271b973d9e5920ed8a23f97a0a5a5d32aa2583
AlexMindrik1994 <49689983+AlexMindrik1994@users.noreply.github.com>
2019-06-03 11:54:51 +0300

    Add files via upload

---
+++ js/homework5.js
@@ -0,0 +1,19 @@
+/*---------------Задача 1----------------------------*/
+
+    let b = [5, 6, 7, 8];
+    let a = [1, 2, 3, 4];
+
+
+function compare(a,b){
+    if (a.length === b.length){ 
+      console.log (true);
+        
+   }else if(a.length != b.length){
+   	 console.log (false);
+   }
+   
+
+}
+compare(a,b); 
+
+/*--------------------------------------------*/
